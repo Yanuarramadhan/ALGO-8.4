b2741ed04bb18868124e5a4220b0b330d931471f
Yanuar <45526227+Yanuarramadhan@users.noreply.github.com>
2019-01-05 11:44:15 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+int CETAK();
+int main();
+{
+    CETAK()
+}
+
+void CETAK()
+{
+    int A,B,T;
+    A=5; B=2;
+    T = A+B;
+    printf("%d", T)
+}
